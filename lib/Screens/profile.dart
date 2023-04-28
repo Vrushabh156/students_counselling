924ddc464eda9e851d3e2900fa8dcc955e36b8cf
Vrushabh Lewade <96383783+Vrushabh132@users.noreply.github.com>
2023-04-28 21:18:18 +0530

    First Commit

--- lib/Screens/profile.dart
+++ lib/Screens/profile.dart
@@ -10,13 +10,52 @@ class ProfilePage1 extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage1> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        padding: EdgeInsets.only(top: 20),
-        child: const Text(
-          '------- Profile -------',
-          style: TextStyle(fontSize: 13, color: Colors.black),
-        ),
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          Container(
+            color: Colors.blue,
+            height: MediaQuery.of(context).size.height * 0.3,
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: Column(
+                children: <Widget>[
+                  Stack(
+                    children: const [
+                      CircleAvatar(
+                        radius: 50,
+                        // backgroundColor: Colors.white,
+                        // backgroundImage: ,
+                      ),
+                      Positioned(
+                          bottom: 5,
+                          right: 5,
+                          child: CircleAvatar(
+                            backgroundColor: Colors.white,
+                            radius: 15,
+                            child: Icon(Icons.edit),
+                          ))
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    "Welcome Amit",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.white),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
